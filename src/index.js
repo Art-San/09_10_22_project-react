78d14e477e06d3d9dff3f308cf4b9b14294ba313
Aleksander <asava@sibmail.com>
2022-10-09 23:17:15 +0700

    this is just the beginning

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter"
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <h1>Hello world</h1>
+        <Counter/>
     </React.StrictMode>
 );
