1da2efac30c2333d8ac487a6412d0ced24d576a8
Aleksander <asava@sibmail.com>
2022-10-10 23:01:11 +0700

    10.10.22

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,20 @@ const Counter = () => {
     const formatCount = () => {
       return  count === 0 ? 'empty' : count
     }
+   
+    const getBageClasses = () => {
+        return count === 0 
+        ? "badge m-2 text-bg-warning" 
+        : 'badge m-2 text-bg-primary'
+    }
+    
+
+    
     return (
         <>
-            <span>{formatCount()}</span>
-            <button>+</button>
+            
+            <span className={getBageClasses()}>{formatCount()}</span>
+            <button className="btn btn-primary btn-sm m-2">+</button>
         </>
     )
     
