a8f1f49514a19ac647032b6c3878c9ce81d561b3
Aleksander <asava@sibmail.com>
2022-10-12 22:57:45 +0700

    12.10.22

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Counter = () => {
-    const count = 0
+    const arr = ['tag1', 'tag2', 'tag3']
+    const [count, setCount] = useState(0)
+    const [tags, setTegs] = useState(arr)
+    
+
     const formatCount = () => {
       return  count === 0 ? 'empty' : count
     }
+   
+    const getBageClasses = () => {
+        return count === 0 
+        ? "badge m-2 text-bg-warning" 
+        : 'badge m-2 text-bg-primary'
+    }
+    
+    const handleIncrement = () => {
+        setCount((prevState) => prevState + 1)
+    }
+
+    const handleDecrement = () => {
+        if(count > 0) {
+            setCount((prevState) => prevState - 1)
+        }
+    } 
+  
+    
+    const handleTagChange = (id) => {
+        console.log('id', id)
+        setTegs((prevItems) => prevItems.filter(tag => tag !== id))
+    }
+    
+    const renderTegs = () => {
+       return tags.length !== 0 && tags.map((tag) => ( 
+            <li 
+                key={tag} 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={() => handleTagChange(tag)}
+            >
+                {tag}
+            </li>
+            
+        )) 
+        
+    }
+
+    if(tags.length !== 0) {
+       return <ul> {renderTegs()}</ul>
+    }
+    
+    
     return (
         <>
-            <span>{formatCount()}</span>
-            <button>+</button>
+            <span className={getBageClasses()}>{formatCount()}</span>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={handleIncrement}
+                >
+                    +
+                </button>
+            <button 
+                className="btn btn-primary btn-sm m-2" 
+                onClick={handleDecrement}
+                >
+                    -
+                </button>
         </>
     )
     
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter"
+import Navbar from "./components/navbar";
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        <Navbar/>
         <Counter/>
     </React.StrictMode>
 );
