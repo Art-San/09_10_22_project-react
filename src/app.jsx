a2835998debb711cadf72a43f124d037a958f3ed
Aleksander <asava@sibmail.com>
2022-10-30 15:06:02 +0700

    30/10/22

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 // import Counter from "./components/counter"
-import CountersList from './components/countersList'
-import Navbar from "./components/navbar";
+// import CountersList from './components/countersList'
+// import Navbar from "./components/navbar";
 import Users from "./components/users";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <CountersList/>
+    {/* <Navbar/> */}
+    {/* <CountersList/> */}
     <Users/>
 </>
   )
