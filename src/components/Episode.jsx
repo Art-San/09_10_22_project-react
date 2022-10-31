133c5af6b5ca2211f0ac63865741b4c4cfc414ee
Aleksander <asava@sibmail.com>
2022-10-31 22:28:06 +0700

    end

--- src/components/Episode.jsx
+++ src/components/Episode.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Episode = (props) => {
-    const { name, airDate, Episode} = props
+    const { name, airDate, episode} = props
+    console.log('name', typeof episode)
   return (
     <div className="col-4 mb-2">
         <div className="card" style={{ height: '100%' }}>
             <div className="card-body">
                 <h5 className="card-title">
-                    {name} {Episode}
+                    {name} {episode}
                 </h5>
                 <h6 className="card-subtitle mb-2 text-muted bg-warning">
                     {airDate}
@@ -18,5 +20,9 @@ const Episode = (props) => {
    
   )
 }
-
+Episode.propTypes = {
+    name: PropTypes.string.isRequired,
+    airDate: PropTypes.string.isRequired,
+    episode: PropTypes.string.isRequired,
+}
 export default Episode
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import _ from "lodash"
+import PropTypes from 'prop-types'
 
 const Pagination = (props) => {
     const { itemsCount, pageSize, onPageChange, currentPage } = props
@@ -31,5 +32,10 @@ const Pagination = (props) => {
     </nav>
   )
 }
-
+Pagination.propTypes ={
+    itemsCount: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired, 
+    currentPage: PropTypes.number.isRequired,
+}
 export default Pagination
