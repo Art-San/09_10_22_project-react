f5f55dc5bfdc9dce8a248ab9c09bee1e27b64897
Aleksander <asava@sibmail.com>
2022-11-05 23:25:11 +0700

    1. Extract table

--- src/app/components/Users.jsx
+++ src/app/components/Users.jsx
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types'
 import React, { useState, useEffect } from 'react'
 import Pagination from './Pagination'
-import User from './User'
 import api from '../api'
 import { paginate } from '../utils/paginate'
 import GroupList from './GroupList'
 import SearchStatus from './SearchStatus'
+import UsersTable from './UsersTable'
 
 const Users = ({ users: allUsers, ...rest }) => {
-    console.log('user.profession', allUsers)
+    // console.log('user.profession', allUsers)
     const [currentPage, setCurrentPage] = useState(1)
     const [profession, setProfession] = useState()
     const [selectedProf, setSelectedProf] = useState()
@@ -29,22 +29,12 @@ const Users = ({ users: allUsers, ...rest }) => {
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex)
     }
-    // const filteredUsers = selectedProf
-    // ? allUsers.filter(
-    //     (user) =>
-    //         JSON.stringify(user.profession) ===
-    //         JSON.stringify(selectedProf)
-    // )
-    // : allUsers
     const filteredUsers = selectedProf
-        ? allUsers.filter((user) => {
-            console.log(user.profession)
-            console.log(selectedProf)
-            console.log(user.profession === selectedProf)
-            console.log('=============')
-            if (user.profession === selectedProf) return true
-            return false
-        })
+        ? allUsers.filter(
+            (user) =>
+                JSON.stringify(user.profession) ===
+                JSON.stringify(selectedProf)
+        )
         : allUsers
 
     const count = filteredUsers.length
@@ -74,24 +64,7 @@ const Users = ({ users: allUsers, ...rest }) => {
                 <div className="d-flex flex-column">
                     <SearchStatus length={count} />
                     {count > 0 && (
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Имя</th>
-                                    <th scope="col">Качества</th>
-                                    <th scope="col">Провфессия</th>
-                                    <th scope="col">Встретился, раз</th>
-                                    <th scope="col">Оценка</th>
-                                    <th scope="col">Избранное</th>
-                                    <th />
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {userCrop.map((user) => (
-                                    <User key={user._id} {...rest} {...user} />
-                                ))}
-                            </tbody>
-                        </table>
+                        <UsersTable users={userCrop} {...rest}/>
                     )}
                     <div className="d-flex justify-content-center">
                         <Pagination
