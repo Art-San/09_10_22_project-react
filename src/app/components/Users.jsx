ff6d4962cda74473e75cce4fec7f6af8c22b8494
Aleksander <asava@sibmail.com>
2022-11-07 00:43:03 +0700

    5. Extracting the TableHeader

--- src/app/components/Users.jsx
+++ src/app/components/Users.jsx
@@ -73,7 +73,7 @@ const Users = ({ users: allUsers, ...rest }) => {
                         <UsersTable
                             users={userCrop}
                             onSort={handleSort}
-                            currentSort={sortBy}
+                            selectedSort={sortBy}
                             {...rest}
                         />
                     )}
