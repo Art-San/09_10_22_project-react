fbeb4c16a7cb1b7e552210ed3a528f83106cce58
Aleksander <asava@sibmail.com>
2022-11-07 17:17:50 +0700

    7. Nested component cell, end

--- src/app/components/TableBody.jsx
+++ src/app/components/TableBody.jsx
@@ -2,26 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import _ from 'lodash'
 
-// const twoDArray = [
-//     [ '[i][j]',  '[i][j+1]',  '...', '[i][j+nj]' ],
-//     ['[i+1][j]','[i+1][j+1]', '...','[i+1][j+nJ]'],
-//     [   '...',      '...',    '...',    '...'    ],
-//     ['[i+1][j]','[i+m][j+1]', '...','[i+m][j+nJ]']
-// ]
-// for (let i = 0; i < twoDArray.length; i++) {
-//     for (let j = 0; j < twoDArray[i].length; j++) {}
-// }
-
 const TableBody = ({ data, columns }) => {
+    const renderContent = (item, column) => {
+        if (columns[column].component) {
+            const component = columns[column].component
+            if (typeof component === 'function') {
+                return component(item)
+            }
+            return component
+        }
+        return _.get(item, columns[column].path)
+    }
     return (
         <tbody>
             {data.map((item) => (
                 <tr key={item._id}>
                     {Object.keys(columns).map((column) => (
-                        <td
-                            key={column}>
-                            {_.get(item, columns[column].path)}
-                        </td> // _.get -- ЭТО ЛОДАШ
+                        <td key={column}>{renderContent(item, column)}</td> // _.get -- ЭТО ЛОДАШ
                     ))}
                 </tr>))}
         </tbody>
