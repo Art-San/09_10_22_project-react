5bde295d27d279841c2954e514b3211e32ccf9c0
Aleksander <asava@sibmail.com>
2022-11-15 12:35:48 +0700

    Added a router to the application

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './app/App'
+import { BrowserRouter } from 'react-router-dom'
+// Добавили роутер к приложению BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>
+)
+
+// Во избежание ошибок React 18 & React Router 5 в index.js 
+// компонент <React.StrictMode> также необходимо переместить внутрь 
+// компонента <BrowserRouter> таким образом:
+
+// {/* <BrowserRouter>
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// </BrowserRouter> */}
