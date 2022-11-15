f5d1b79b88a82e4fbfc41bee5f7f6113e032ecfb
Aleksander <asava@sibmail.com>
2022-11-15 13:33:45 +0700

    navbar working

--- src/app/App.js
+++ src/app/App.js
@@ -3,10 +3,12 @@ import { Redirect, Route, Switch } from 'react-router-dom'
 import Users from './layouts/Users'
 import Login from './layouts/Login'
 import Main from './layouts/Main'
+import NavBar from './components/NavBar'
 
 function App() {
     return (
         <>
+            <NavBar/>
             <Switch>
                 <Route path='/users' component={Users}/>
                 <Route path='/login' component={Login}/>
---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,21 @@
+// https://getbootstrap.com/docs/5.2/components/navs-tabs/#base-nav
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <ul className="nav">
+            <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">Main</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/users">Users</Link>
+            </li>
+        </ul>
+    )
+}
+
+export default NavBar
