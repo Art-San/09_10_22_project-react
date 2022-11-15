03d6c4465b92f9e5e710bbd7e3b2c93a98603f79
Aleksander <asava@sibmail.com>
2022-11-15 20:28:16 +0700

    user-specific page implementation

--- src/app/components/UserPage.jsx
+++ src/app/components/UserPage.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import api from '../api'
+import QualitiesList from './QualitiesList'
+import { useHistory } from 'react-router-dom'
 
 const UserPage = ({ userId }) => {
-    return (
-        <h1>UserPage {userId}</h1>
-    )
+    const history = useHistory()
+    console.log('userId', userId)
+    const [user, setUser] = useState()
+    useEffect(() => {
+        api.users.getById(userId).then((data) => setUser(data))
+    })
+    const handleClick = () => {
+        history.push('/users')
+    }
+    if (user) {
+        return (
+            <div>
+                <h1>{user.name}</h1>
+                <h2>Профессия: {user.profession.name}</h2>
+                <QualitiesList qualities={user.qualities}/>
+                <p>CompletedMeetings: {user.completedMeetings}</p>
+                <h2>Rate: {user.rate}</h2>
+                <button onClick={handleClick} >Все пользователи</button>
+            </div>
+        )
+    } else {
+        return <h1>Loading</h1>
+    }
 }
 UserPage.propTypes = {
     userId: PropTypes.string.isRequired
