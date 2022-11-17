c40e8e39e7f69921b6a56fda3e40b03b42584c40
Aleksander <asava@sibmail.com>
2022-11-17 19:47:17 +0700

    end

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" id='emaii' />
+            </div>
+            <div>
+                <label htmlFor="password">Пароль</label>
+                <input type="password" id='password' />
+            </div>
+        </form>
     )
 }
 
