4c678448165f6cc2b427d53a1c3687f5175067f4
Aleksander <asava@sibmail.com>
2022-11-18 23:22:24 +0700

    Basic validation

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import TextField from '../components/TextField'
-
+// 18.11.22
 const Login = () => {
     const [data, setData] = useState({ email: '', password: '' })
     const [, setErrors] = useState()
