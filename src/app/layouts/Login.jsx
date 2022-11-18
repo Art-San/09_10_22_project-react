eaa1aae909787a94c9862706cb9bb9da0b7b895e
Aleksander <asava@sibmail.com>
2022-11-18 20:37:44 +0700

    Processing a form submission

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import TextField from '../components/TextField'
-
+// 18/11/22
 const Login = () => {
     const [data, setData] = useState({ email: '', password: '' })
     const handleChange = ({ target }) => {
