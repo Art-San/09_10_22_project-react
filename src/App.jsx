e2ac29c88b7ae8f76bf473f091ace384668c13fe
Aleksander <asava@sibmail.com>
2022-11-23 00:09:06 +0700

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MyForm from './components/MyForm'
 // import Counter from './components/counter'
 // import EpisodesList from './components/EpisodesList'
 import ProductList from './components/ProductList'
@@ -6,6 +7,7 @@ import ProductList from './components/ProductList'
 const App = () => {
     return (
         <>
+            <MyForm/>
             {/* <Counter/> */}
             <div className="App">
                 {/* <EpisodesList /> */}
