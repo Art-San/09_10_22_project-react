f99d83611d8c241b3fe391814f7b44b50db67dc8
Aleksander <asava@sibmail.com>
2022-11-26 20:15:10 +0700

    pause

--- src/app/components/ui/RegisterForm.jsx
+++ src/app/components/ui/RegisterForm.jsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import TextField from '../common/form/TextField'
 import { validator } from '../../utils/validator'
+import api from '../../api'
+// import { professions } from '../../api/fake.api/professions.api'
 
 const RegisterForm = () => {
-    const [data, setData] = useState({ email: '', password: '' })
+    const [data, setData] = useState({ email: '', password: '', profession: '' })
     const [errors, setErrors] = useState({})
+    const [professions, setProfession] = useState()
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => setProfession(data))
+    }, [])
+    useEffect(() => {
+        console.log('professions', professions)
+    }, [professions])
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
@@ -74,7 +83,36 @@ const RegisterForm = () => {
                 onChange={handleChange}
                 error={errors.password}
             />
-            {/* <button>Submit</button>  */}
+            <div className="md-4">
+                <label htmlFor="validationCustom04" className="form-label">
+                    State
+                </label>
+                <select
+                    className="form-select"
+                    id="validationCustom04"
+                    required
+                >
+                    <option selected={data.profession === ''} disabled value="">
+                        Choose...
+                    </option>
+                    {professions &&
+                        Object.keys(professions).map((professionName) => (
+                            <option
+                                selected={
+                                    professions[professionName]._id ===
+                                    data.profession
+                                }
+                                value={professions[professionName]._id}
+                                key={professions[professionName]._id}
+                            >
+                                {professions[professionName].name}
+                            </option>
+                        ))}
+                </select>
+                <div className="invalid-feedback">
+                    Please select a valid state.
+                </div>
+            </div>
             <button
                 type='submit'
                 disabled={!isValid}
@@ -86,3 +124,25 @@ const RegisterForm = () => {
 }
 
 export default RegisterForm
+
+// 8:19
+
+//   {/* <select
+//                     className="form-select"
+//                     id="validationCustom04"
+//                     required
+//                 >
+//                     <option selected={data.profession === ''} disabled value="">
+//                         Choose...
+//                     </option>
+//                     {professions &&
+//                         professions.map((profession) => (
+//                             <option
+//                                 selected={profession._id === data.profession}
+//                                 value={profession._id}
+//                                 key={profession._id}
+//                             >
+//                                 {profession.name}
+//                             </option>
+//                         ))}
+//                 </select> */}
