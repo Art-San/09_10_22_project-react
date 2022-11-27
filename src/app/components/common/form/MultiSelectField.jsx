dab2791fdf605d758b4d1d2b1bb5fb9e1d9a7057
Aleksander <asava@sibmail.com>
2022-11-28 00:34:46 +0700

    Множественный select

--- src/app/components/common/form/MultiSelectField.jsx
+++ src/app/components/common/form/MultiSelectField.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Select from 'react-select'
 import PropTypes from 'prop-types'
-
+// https://react-select.com/home
 const MultiSelectField = ({ options, onChange, name, label }) => {
     const optionsArray =
     !Array.isArray(options) && typeof (options) === 'object'
